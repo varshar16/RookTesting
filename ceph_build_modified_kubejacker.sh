ed189cde430e78f73511b61763fd7464c9edac63
Varsha <rvarsha016@gmail.com>
2020-12-07 16:20:46 +0530

    Update ceph_build_modified_kubejacker.sh

--- ceph_build_modified_kubejacker.sh
+++ ceph_build_modified_kubejacker.sh
@@ -5,6 +5,7 @@ git clone --recursive https://github.com/ceph/ceph.git
 cd ~/ceph
 git submodule update --force --init --recursive
 git remote add vr https://github.com/varshar16/ceph.git
+git fetch vr
 git checkout -b vr/rook-testing
 git rebase origin/master
 ./install-deps.sh
